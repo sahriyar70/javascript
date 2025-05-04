00e2027a55cb69f135e0c614423fcad428dfdb02
AL-Sahriyar <alsahriyar229@gmail.com>
2025-05-04 23:11:22 +0600

    for and while

---
+++ forloop.js
@@ -0,0 +1,24 @@
+for ( var i = 0; i < 100 ; i ++){
+    console.log((i + 1) + "sahriyar")
+}
+
+for ( var i = 
+    0; i < 100 ; i ++){
+    if (i%2==0){
+        console.log((i)+"odd" )
+
+    } 
+        
+    
+}
+
+var sum = 0;
+for(i = 0; i <= 10; i ++){
+    console.log (sum + "+" + i + "=" + (sum+i) )
+    sum += i
+
+    
+}
+
+console.log("result"+ sum)
+
