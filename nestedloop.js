68a09dbec5537ac1c08da0ba3f34aee8aefa2da0
AL-Sahriyar <alsahriyar229@gmail.com>
2025-05-12 19:18:16 +0600

    nested

---
+++ nestedloop.js
@@ -0,0 +1,18 @@
+var n = 10
+for(var i = 1 ; i <= n ; i ++){
+    var result = " "
+    for(var j = 1 ; j<=i ; j++)
+        result += j+ ' '  // use "*"
+    console.log(result)
+}
+
+
+var m = 10
+for( var i = 1 ; i <=m ; i ++){
+    result = ""
+    for( var j = 1; j<=m; j++){
+            result += "* " // use j+
+    }
+    console.log(result)
+        
+}
