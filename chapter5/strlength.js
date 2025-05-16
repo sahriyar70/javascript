dbaf8020c34727628801963f4f9fff2615dd2e9c
AL-Sahriyar <alsahriyar229@gmail.com>
2025-05-16 11:43:50 +0600

    40

---
+++ chapter5/strlength.js
@@ -0,0 +1,13 @@
+var a = 'sahriyar'
+var length = 0
+
+console.log(a.length)
+
+while (true){
+    if (a.charAt(length)== '') {
+        break
+    }else{
+        length++
+    }
+}
+console.log(length)
---
+++ chapter5/strmathods.js
@@ -0,0 +1,18 @@
+var a = " i am "
+var b = "     SAHRIYAR"
+
+var c = a.concat(b)
+console.log(c)
+
+var d = a.startsWith("i am")
+console.log(d)
+var e = b.endsWith('')
+
+var f = a.toUpperCase()
+console.log(f)
+
+var g = b.toLocaleLowerCase()
+console.log(g)
+
+var h = b.trim()
+console.log(h)
